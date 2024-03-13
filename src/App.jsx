ba587c849612012fe01d1266b08f3160599bb525
Yamin39 <mdmufazzalhossainyamin5010@gmail.com>
2024-03-14 01:12:45 +0600

    project initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="w-10/12 mx-auto max-w-[1440px] font-exo">
+      <h1>Knowledge Cafe</h1>
+    </div>
+  );
+}
+
+export default App;
